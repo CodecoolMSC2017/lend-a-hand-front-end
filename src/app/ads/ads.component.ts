a2fd56c422fd31921809b2b5f55b9f27669f90d4
AlexaPekar <missdreambelle@gmail.com>
2018-07-19 12:11:51 +0200

    Implement basic ads.component.ts and ads.component.html

--- src/app/ads/ads.component.ts
+++ src/app/ads/ads.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AdService} from '../ad.service';
+import {Router} from '@angular/router';
+import {Ad} from '../ad.model';
 
 @Component({
-  selector: 'app-ads',
-  templateUrl: './ads.component.html',
-  styleUrls: ['./ads.component.css']
+    selector: 'app-ads',
+    templateUrl: './ads.component.html',
+    styleUrls: ['./ads.component.css']
 })
 export class AdsComponent implements OnInit {
 
+    ads: Ad[];
 
-  constructor() { }
+    constructor(private router: Router, private adService: AdService) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.ads = this.adService.getAds();
+        this.adService.ads.subscribe(ads => this.ads = ads);
+    }
+
 
 }
