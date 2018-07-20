0f61271757efca33d546466905113f0e8da5cfd8
Farago Bence <semmiertelme13@gmail.com>
2018-07-20 07:57:07 +0200

    App component's header moved to main-page header.

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -16,25 +16,23 @@ export class MainPageComponent implements OnInit {
     user = {};
     categories = ['All', 'Babysitting', 'IT', 'Garden', 'Learning', 'Building'];
 
-    constructor(private adService:AdService,private mainPageService: MainPageService, private authService: AuthorizationService, private router: Router,private gem:GlobalEventManagerService) {
+    constructor(private adService: AdService, private mainPageService: MainPageService, private authService: AuthorizationService, private router: Router, private gem: GlobalEventManagerService) {
     }
 
     ngOnInit() {
         if (sessionStorage.getItem('user') != null) {
             this.user = JSON.parse(sessionStorage.getItem('user'));
         }
-        console.log(this.user);
-        
-        this.adService.getAds().subscribe(ads =>{
-            this.ads=ads;
-        })
+        this.adService.getAds().subscribe(ads => {
+            this.ads = ads;
+        });
         this.gem.categoryFilterEmitter.subscribe(category => {
-            if(category){
-            this.adService.getAdsByCategory(category).subscribe(ads =>{
-                this.ads=ads;
-            });
-        }
-        })
+            if (category) {
+                this.adService.getAdsByCategory(category).subscribe(ads => {
+                    this.ads = ads;
+                });
+            }
+        });
 
     }
 
