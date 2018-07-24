c4fa06682da72b38c13bb9f5dee57b73c8a5ffe6
Farago Bence <semmiertelme13@gmail.com>
2018-07-24 16:16:08 +0200

    More profile front end formating made ,now fields are changable.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -31,4 +31,89 @@ export class ProfileComponent implements OnInit {
         }
         */
     }
+
+    profileChanges(): void {
+        this.showFullNameInput();
+        this.showPhoneInput();
+        this.showLocationInput();
+        this.disableProfileChangeButton();
+        this.showChangeButton();
+    }
+
+
+    showFullNameInput(): void {
+        const fullNameElements = document.getElementsByClassName('fullname');
+        for (const e of fullNameElements) {
+            e.classList.add('hidden');
+        }
+
+        const inputElement = document.createElement('input');
+        inputElement.type = 'text';
+        inputElement.setAttribute('id', 'full-name-input');
+        inputElement.placeholder = 'Full Name';
+
+        const contentDiv = document.getElementById('profile-div').firstElementChild;
+        contentDiv.appendChild(inputElement);
+    }
+
+    showPhoneInput(): void {
+        const fullNameElements = document.getElementsByClassName('phone');
+        for (const e of fullNameElements) {
+            e.classList.add('hidden');
+        }
+        const inputElement = document.createElement('input');
+        inputElement.type = 'text';
+        inputElement.setAttribute('id', 'phone-input');
+        inputElement.placeholder = 'Phone number';
+
+        const phoneTd = document.getElementById('phone-td');
+        phoneTd.appendChild(inputElement);
+    }
+
+    showLocationInput(): void {
+        const fullNameElements: HTMLCollectionOf<Element> = document.getElementsByClassName('address');
+        for (const e of fullNameElements) {
+            e.classList.add('hidden');
+        }
+        const postalCodeInputElement = document.createElement('input');
+        postalCodeInputElement.type = 'text';
+        postalCodeInputElement.setAttribute('id', 'postal-code-input');
+        postalCodeInputElement.placeholder = 'Postal code';
+
+        const cityInputElement = document.createElement('input');
+        cityInputElement.type = 'text';
+        cityInputElement.setAttribute('id', 'city-input');
+        cityInputElement.placeholder = 'City';
+
+        const addressInputElement = document.createElement('input');
+        addressInputElement.type = 'text';
+        addressInputElement.setAttribute('id', 'address-input');
+        addressInputElement.placeholder = 'Address';
+
+        const locationTd = document.getElementById('address-td');
+        locationTd.appendChild(postalCodeInputElement);
+        locationTd.appendChild(document.createElement('br'));
+        locationTd.appendChild(cityInputElement);
+        locationTd.appendChild(document.createElement('br'));
+        locationTd.appendChild(addressInputElement);
+    }
+
+    disableProfileChangeButton(): void {
+        document.getElementById('profile-change-button').classList.add('hidden');
+    }
+
+    showChangeButton(): void {
+
+        const updateButton = document.createElement('button');
+        updateButton.setAttribute('id', 'update-button');
+        updateButton.innerText = 'Update';
+        updateButton.addEventListener('click', this.update);
+
+        const contentDiv = document.getElementById('profile-div').firstElementChild;
+        contentDiv.appendChild(updateButton);
+    }
+
+    update(): void {
+        console.log('ANYÁD!');
+    }
 }
