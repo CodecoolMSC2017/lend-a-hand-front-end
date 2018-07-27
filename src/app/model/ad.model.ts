a0b590c72c8e220fe011584930092918254b4778
AlexaPekar <missdreambelle@gmail.com>
2018-07-27 14:46:51 +0200

    Fix fields of ad.model.ts

--- src/app/model/ad.model.ts
+++ src/app/model/ad.model.ts
@@ -1,13 +1,13 @@
-import {User} from './user.model';
-
 export class Ad {
     id: number;
-    advertiser: User;
-    chosenApplicant: User;
-    applications = [];
     title: string;
     description: string;
     payment: number;
     category: string;
     isPremium: boolean;
+    timestamp: Date;
+    type: string;
+    advertiserId: number;
+    advertiserName: string;
+    pictureLink: string;
 }
