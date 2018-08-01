0eba4d5ab83058487330894d9476027f3e289a2e
Farago Bence <semmiertelme13@gmail.com>
2018-08-01 14:15:53 +0200

    Implemented application listing.

--- src/app/applications/applications.component.ts
+++ src/app/applications/applications.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Application} from '../model/application.model';
+import {GlobalEventManagerService} from '../service/global-event-manager.service';
 
 @Component({
     selector: 'app-applications',
@@ -7,10 +9,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ApplicationsComponent implements OnInit {
 
-    constructor() {
+    applications: Application[];
+
+    constructor(private gem: GlobalEventManagerService) {
     }
 
     ngOnInit() {
+        this.gem.applicationsEmitter.subscribe(applications => {
+            if (applications) {
+                this.applications = applications;
+                sessionStorage.setItem('applications', JSON.stringify(applications));
+            } else {
+                this.applications = JSON.parse(sessionStorage.getItem('applications'));
+            }
+        });
     }
 
 }
