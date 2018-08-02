356a9e8e19566a012faf729bfd18b33c2bb97655
Farago Bence <semmiertelme13@gmail.com>
2018-08-02 23:48:19 +0200

    Application's formated under the single ad.

--- src/app/single-ad/single-ad.component.ts
+++ src/app/single-ad/single-ad.component.ts
@@ -56,8 +56,8 @@ export class SingleAdComponent implements OnInit, OnDestroy {
         }
 
         this.appService.getApplicationsByAd(this.ad.id).subscribe(applications => {
-            console.log(this.ad.id);
-            this.applications = applications;
+            this.applications = this.formatApps(applications);
+            console.log(this.applications);
         });
 
     }
@@ -125,6 +125,56 @@ export class SingleAdComponent implements OnInit, OnDestroy {
 
     standBy(id) {
         (<HTMLImageElement>document.getElementById(id)).src = 'http://www.bsmc.net.au/wp-content/uploads/No-image-available.jpg';
+    }
+
+    formatAdTimestamp(timestamp: string): string {
+        let formattedTimestamp = '';
+        let splittedTimestamp = (timestamp + '').split(',');
+        formattedTimestamp = formattedTimestamp + this.formatAdsTimestamp(timestamp) + ' ';
+        if (splittedTimestamp[3].length < 2) {
+            formattedTimestamp = formattedTimestamp + '0' + splittedTimestamp[3] + ':';
+        } else {
+            formattedTimestamp = formattedTimestamp + splittedTimestamp[3] + ':';
+        }
+        if (splittedTimestamp[4].length < 2) {
+            formattedTimestamp = formattedTimestamp + '0' + splittedTimestamp[4];
+        } else {
+            formattedTimestamp = formattedTimestamp + splittedTimestamp[4];
+        }
+        return formattedTimestamp;
+    }
+
+    formatAdsTimestamp(timestamp: string): string {
+        let formattedTimestamp = '';
+        let splittedTimestamp = (timestamp + '').split(',');
+        formattedTimestamp = formattedTimestamp + splittedTimestamp[0] + '.';
+        if (splittedTimestamp[1].length < 2) {
+            formattedTimestamp = formattedTimestamp + '0' + splittedTimestamp[1] + '.';
+        } else {
+            formattedTimestamp = formattedTimestamp + splittedTimestamp[1] + '.';
+        }
+        if (splittedTimestamp[2].length < 2) {
+            formattedTimestamp = formattedTimestamp + '0' + splittedTimestamp[2] + '.';
+        } else {
+            formattedTimestamp = formattedTimestamp + splittedTimestamp[2] + '.';
+        }
+        return formattedTimestamp;
+    }
+
+    formatApps(applications: Application[]): Application[] {
+        const formattedApps = [];
+        for (let i = 0; i < applications.length; i++) {
+            const application = applications[i];
+            if (application.message.length > 85) {
+                application.formattedMessage = application.message.substring(0, 85) + '...';
+            } else {
+                application.formattedMessage = application.message;
+            }
+            application.formattedTimestamp = this.formatAdsTimestamp(application.timestamp);
+            application.timestamp = this.formatAdTimestamp(application.timestamp);
+            formattedApps.push(application);
+        }
+        return formattedApps;
     }
 
 
