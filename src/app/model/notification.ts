3bc076bc1d2b95665806aec2d7facae9964cdca1
Farago Bence <semmiertelme13@gmail.com>
2018-08-14 15:00:35 +0200

    Implemented notification model in frontend

---
+++ src/app/model/notification.ts
@@ -0,0 +1,22 @@
+import {Rating} from './rating.model';
+import {Application} from './application.model';
+import {Ad} from './ad.model';
+
+
+export class Notification {
+    id: number;
+    fromId: number;
+    fromName: string;
+    toId: number;
+    toName: string;
+    ad: Ad;
+    application: Application;
+    employeeRating: Rating;
+    employerRating: Rating;
+    text: string;
+    type: string;
+    read: boolean;
+    timestamp: string;
+    formattedTimestamp: string;
+
+}
