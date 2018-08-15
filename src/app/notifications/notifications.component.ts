b8937f004234616a4434cf5d205823bc7c1ad83c
Farago Bence <semmiertelme13@gmail.com>
2018-08-15 08:21:37 +0200

    Added basic formatting  to notifications

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -3,6 +3,10 @@ import {User} from '../model/user.model';
 import {GlobalEventManagerService} from '../service/global-event-manager.service';
 import {NotificationService} from '../service/notification.service';
 import {Router} from '@angular/router';
+import {Notification} from '../model/notification';
+import {Ad} from '../model/ad.model';
+import {ApplicationService} from '../service/application.service';
+import {Application} from '../model/application.model';
 
 @Component({
     selector: 'app-notifications',
@@ -15,7 +19,7 @@ export class NotificationsComponent implements OnInit {
     notifications: Notification[];
     error: string;
 
-    constructor(private gem: GlobalEventManagerService, private notificationService: NotificationService, private router: Router) {
+    constructor(private gem: GlobalEventManagerService, private notificationService: NotificationService, private applicationService: ApplicationService, private router: Router) {
     }
 
     ngOnInit() {
@@ -23,10 +27,108 @@ export class NotificationsComponent implements OnInit {
         this.gem.updateUser(this.user);
 
         this.notificationService.getAllNotificationsByUser(this.user.id).subscribe(notifications => {
-            this.notifications = notifications;
+            this.notifications = this.formatNotifications(notifications);
         }, error => {
             this.handleError(error);
         });
+    }
+
+    read(id: number) {
+        const noteTd = document.getElementById(id.toString());
+        noteTd.children.item(1).classList.remove('hidden');
+        noteTd.children.item(2).classList.remove('hidden');
+        const close = (event) => this.close(id);
+        const read = (event) => this.read(id);
+        noteTd.removeEventListener('click', read);
+        noteTd.addEventListener('click', close);
+        this.notificationService.readNotification(id).subscribe(notification => {
+            noteTd.classList.remove('false');
+        }, error => {
+            this.handleError(error);
+        });
+    }
+
+    close(id: number) {
+        const noteTd = document.getElementById(id.toString());
+        noteTd.children.item(1).classList.add('hidden');
+        noteTd.children.item(2).classList.add('hidden');
+        const close = (event) => this.close(id);
+        const read = (event) => this.read(id);
+        noteTd.removeEventListener('click', close);
+        noteTd.addEventListener('click', read);
+    }
+
+    toAd(ad: Ad) {
+        this.gem.updateSingleAd(ad);
+        this.router.navigate(['ad']);
+    }
+
+    toMessages() {
+        this.router.navigate(['messages']);
+    }
+
+    toMyApplications() {
+        this.applicationService.getApplicationsByApplicantId(this.user.id).subscribe(applications => {
+            this.gem.updateApplications(applications);
+            this.router.navigate(['applications']);
+        }, error => {
+            this.handleError(error);
+        });
+    }
+
+    toRate(application: Application) {
+        this.gem.updateApplication(application);
+        this.router.navigate(['rate']);
+    }
+
+    toMyRaters() {
+        this.gem.updateRatingType('myRatings');
+        this.router.navigate(['ratings']);
+    }
+
+    formatNotificationShortTimestamp(timestamp: string): string {
+        let formattedTimestamp = '';
+        const splittedTimestamp = (timestamp + '').split(',');
+        formattedTimestamp = formattedTimestamp + splittedTimestamp[0] + '.';
+        if (splittedTimestamp[1].length < 2) {
+            formattedTimestamp = formattedTimestamp + '0' + splittedTimestamp[1] + '.';
+        } else {
+            formattedTimestamp = formattedTimestamp + splittedTimestamp[1] + '.';
+        }
+        if (splittedTimestamp[2].length < 2) {
+            formattedTimestamp = formattedTimestamp + '0' + splittedTimestamp[2] + '.';
+        } else {
+            formattedTimestamp = formattedTimestamp + splittedTimestamp[2] + '.';
+        }
+        return formattedTimestamp;
+    }
+
+    formatNotificationTimestamp(timestamp: string): string {
+        let formattedTimestamp = '';
+        const splittedTimestamp = (timestamp + '').split(',');
+        formattedTimestamp = formattedTimestamp + this.formatNotificationShortTimestamp(timestamp) + ' ';
+        if (splittedTimestamp[3].length < 2) {
+            formattedTimestamp = formattedTimestamp + '0' + splittedTimestamp[3] + ':';
+        } else {
+            formattedTimestamp = formattedTimestamp + splittedTimestamp[3] + ':';
+        }
+        if (splittedTimestamp[4].length < 2) {
+            formattedTimestamp = formattedTimestamp + '0' + splittedTimestamp[4];
+        } else {
+            formattedTimestamp = formattedTimestamp + splittedTimestamp[4];
+        }
+        return formattedTimestamp;
+    }
+
+    formatNotifications(notifications: Notification[]): Notification[] {
+        const formattedNotifications = [];
+        for (let i = 0; i < notifications.length; i++) {
+            const notification = notifications[i];
+            notification.formattedTimestamp = this.formatNotificationTimestamp(notification.timestamp);
+            notification.timestamp = this.formatNotificationTimestamp(notification.timestamp);
+            formattedNotifications.push(notification);
+        }
+        return formattedNotifications;
     }
 
 
