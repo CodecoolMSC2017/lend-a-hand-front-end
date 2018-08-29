3f537aef160dfa1e8f11ae4dc275a7fbd96259fb
alexapekar <LolkaBolka72>
2018-08-29 12:12:07 +0200

    Fix naming files saved to Amazon S3 Bucket

--- src/app/service/upload-file.service.ts
+++ src/app/service/upload-file.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpEvent, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpEvent, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -12,11 +12,12 @@ export class UploadFileService {
 
     pushFileToStorage(file: File, userName: string): Observable<HttpEvent<{}>> {
         const formdata: FormData = new FormData();
-        const fileName = userName + new Date().valueOf();
+        const extension = file.name.split('.').pop();
+        const fileName = userName + new Date().valueOf().toString();
 
         formdata.append('file', file);
 
-        const req = new HttpRequest('POST', '/api/file/upload/' + fileName, formdata, {
+        const req = new HttpRequest('POST', '/api/file/upload/' + fileName + '/' + extension, formdata, {
             reportProgress: true,
             responseType: 'text'
         });
